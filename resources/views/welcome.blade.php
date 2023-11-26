bfaeb90cf26e0ec31d2fbfb4f330b96cfefd297f
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2023-11-25 16:12:53 -0800

    Creacion navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SIE Home</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1 class='text-orange-200'>Este es un H1</h1>
-    <h2 class='text-orange-600'>Este es un H2</h2>
-    <h3>Este es un H3</h3>
-    <h4>Este es un H4</h4>
+    <nav class="bg-white border-gray-200">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-2">
+            <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="logoSie.jpeg" class="h-14" alt="SIE Logo" />
+            </a>
+            <div class="flex items-center space-x-6 rtl:space-x-reverse">
+                <a href="tel:5541251234" class="text-sm text-gray-500 hover:underline">(555) 412-1234</a>
+                <a href="#" class="text-sm  text-blue-600 hover:underline">Login</a>
+            </div>
+        </div>
+    </nav>
+
+    <nav class="bg-gray-50 dark:bg-gray-700">
+        <div class="max-w-screen-xl px-4 py-3 mx-auto">
+            <div class="flex items-center">
+                <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 </body>
 </html>
