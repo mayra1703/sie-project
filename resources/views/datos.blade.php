6c183ee7c51949eee08475af7f2aa7bbca12f3cd
Magi3654 <magi3654@gmail.com>
2023-12-11 14:15:08 -0800

    dashboard integration

--- resources/views/datos.blade.php
+++ resources/views/datos.blade.php
@@ -12,8 +12,8 @@
         <nav classname='relative top-0 bottom-0 h-100 w-200 bg-white overflow-hidden shadow-stone-950 '>
             <ul>
                 <li><a href="#" class="logo">
-                    <img src="../logoSie.jpeg" alt="">
-                     <span class="nav-item">Dashboard</span>
+                    <img src="../iconSie.jpeg" alt="">
+                     <span class="nav-item">Panel de Inicio</span>
                 </a></li>
                 <li><a href=""><i class="fas fa-home"></i>
                     <span class="nav-item">Inicio</span></a></li>
@@ -21,29 +21,40 @@
                     <i class="fas fa-kardex"></i>
                     <span class="nav-item">Kardex</span>
                 </a></li>
-                <li><a href="">
+                <li><a href="#">
                     <i class="fas fa-wallet"></i>
                     <span class="nav-item">Pagos</span>
                 </a></li>
-                <li><a href="">
+                <li><a href="#">
                     <i class="fas fa-grades"></i>
                     <span class="nav-item">Calificaciones</span>
                 </a></li>
-                <li><a href="">
+                <li><a href="#">
                     <i class="fas fa-schedule"></i>
                    <span class="nav-item">Horario</span>
                 </a></li>
-                <li><a href="">
+                <li><a href="#">
                     <i class="fas fa-user"></i>
                     <span class="nav-item">Perfil</span>
                 </a></li>
-                <li><a href="">
+                <li><a href="#">
                     <i class="fas fa-logout"></i>
                     <span class="nav-item">Cerrar Sesion</span>
                 </a></li>
                 <li><a href=""></a></li>
             </ul>
         </nav>
+        <section class="main">
+            <div class="main-top">
+                <h1>Datos Personales</h1>
+                <i class="fas fa-user-cong"></i>
+            </div>
+            <div class="main-datos">
+                <div class="card">
+                    
+                </div>
+            </div>
+        </section>
     </div>
     
 </body>
