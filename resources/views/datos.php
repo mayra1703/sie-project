4c422a30cddde19c3e04d6e51fce6348d3c2f434
Magi3654 <magi3654@gmail.com>
2023-12-11 12:08:17 -0800

    checking css

--- resources/views/datos.php
+++ resources/views/datos.php
@@ -3,9 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
     <title>Datos Personales</title>
 </head>
 <body>
-    <h1>Esta es la pagina de datos personales</h1>
+    <div class="all">
+        <div class="container">
+            <nav classname='relative top-0 bottom-0 h-100 w-200 bg-white overflow-hidden shadow-stone-950 '>
+                <ul>
+                    <li><a href="#" class="logo">
+                        <img src="../logoSie.jpeg" alt="">
+                        <span class="nav-item">Dashboard</span>
+                    </a></li>
+                    <li><a href=""><i class="fas fa-home"></i>
+                        <span class="nav-item">Inicio</span></a></li>
+                    <li><a href="#">
+                        <i class="fas fa-kardex"></i>
+                        <span class="nav-item">Kardex</span>
+                    </a></li>
+                    <li><a href="">
+                        <i class="fas fa-wallet"></i>
+                        <span class="nav-item">Pagos</span>
+                    </a></li>
+                    <li><a href="">
+                        <i class="fas fa-grades"></i>
+                        <span class="nav-item">Calificaciones</span>
+                    </a></li>
+                    <li><a href="">
+                        <i class="fas fa-schedule"></i>
+                        <span class="nav-item">Horario</span>
+                    </a></li>
+                    <li><a href="">
+                        <i class="fas fa-user"></i>
+                        <span class="nav-item">Perfil</span>
+                    </a></li>
+                    <li><a href="">
+                        <i class="fas fa-logout"></i>
+                        <span class="nav-item">Cerrar Sesion</span>
+                    </a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    
+
 </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
     <title>Inicio de sesion</title>
-    <link rel="stylesheet"  type="text/css" href="../app.css"> 
 </head>
 <body>
     <div class="body">
