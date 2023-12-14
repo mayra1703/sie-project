05c4408ac14e54f397faf4246cfb877092ffe532
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2023-12-14 00:05:44 -0800

    h-screen dashboard

--- resources/views/datos.blade.php
+++ resources/views/datos.blade.php
@@ -8,13 +8,12 @@
     <title>Datos Personales</title>
 </head>
 
-<body class='h-screen '>
-    <div class='flex h-screen'>
-        <nav class='h-full w-60 bg-gradient-to-b from-dark-blue to-light-blue text-white font-semibold'>
+<body class="h-screen">
+    <div class="flex h-screen">
+        <nav class='h-full w-60 bg-gradient-to-b from-dark-blue via-light-blue to-turquese text-white font-bold'>
             <ul class="ml-2 mb-2 mt-2 w-60 p-2">
-
-                <li class="flex w-full text-center justify-center ">
-                    <img src="../imagenes/userProfile.png" alt="" class="flex text-center w-31 h-24 justify-center" />
+                <li class="flex w-full text-center justify-center">
+                    <img src="../imagenes/userProfile.png" alt="" class="flex justify-center text-center w-31 h-24" />
                 </li>
                 <span class="flex justify-center text-center m-2">User</span>
 
@@ -24,55 +23,62 @@
                         <span class="nav-item">Datos Generales</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
-                    <a href="#calificaciones">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
+                    <a href="#" class="nav-link" id="calificaciones">
                         <i class="fas fa-kardex"></i>
                         <span class="nav-item">Calificaciones</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
                     <a href="#grupos">
                         <i class="fas fa-wallet"></i>
                         <span class="nav-item">Grupos Actuales</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
                     <a href="#horario">
                         <i class="fas fa-grades"></i>
                         <span class="nav-item">Horario</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
                     <a href="#kardex">
                         <i class="fas fa-schedule"></i>
                         <span class="nav-item">Kardex</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
-                    <a href="#pagos">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
+                    <a href="#">
                         <i class="fas fa-user"></i>
                         <span class="nav-item">Pagos de Servicios</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
-                    <a href="#evaluacion">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
+                    <a href="#" src="{{ route('evaluacion') }}">
                         <i class="fas fa-user"></i>
                         <span class="nav-item">Evaluacion Docente</span>
                     </a>
                 </li>
-                <li class="hover:bg-white hover:text-dark-blue p-2 rounded-s-lg">
-                    <a href="#salir">
+
+                <li class="hover:bg-white hover:text-dark-blue p-2 mt-4 rounded-s-lg">
+                    <a href="{{ route('welcome') }}">
                         <i class="fas fa-logout"></i>
                         <span class="nav-item">Cerrar Sesion</span>
                     </a>
                 </li>
             </ul>
         </nav>
-
-        <div class="w-full flex m-3" id="contenedor">
-            <iframe src="{{route('calificaciones')}}" class="flex-grow"></iframe>
+        
+        <div class="m-3 w-full flex" id="contenedor">
+            <iframe src="{{ route('calificaciones') }}" class="flex-grow"></iframe>
         </div>
     </div>
-
+    
 </body>
 </html>
