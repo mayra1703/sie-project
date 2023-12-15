207a3139b564f88be48d3791f600c806e02c0d99
Magi3654 <magi3654@gmail.com>
2023-12-15 01:21:18 -0800

    personales, evaluacion, grupos views done

--- resources/views/iframe/personales.blade.php
+++ resources/views/iframe/personales.blade.php
@@ -3,9 +3,107 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/csslogin.css') }}">
     <title>Personales</title>
 </head>
 <body>
-    <h1>Esta es la pagina de personales</h1>
+    <div class="bg-slate-100">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:text-center">
+                <h2 class="text-base font-semibold leading-7 text-dark-blue text-4xl">Bienvenido al SIE</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight text-gold sm:text-4xl">Datos generales</p>
+                <div class="flex justify-evenly uppercase font-bold mt-4">
+                    <p class="mt-2 text-lg leading-8 text-dark-blue">Tecnologico Nacional de Mexico </p>
+                    <p class="mt-2 text-lg leading-8 text-dark-blue">Instituto Tecnologico de Ensenada </p>
+                    <p class="mt-2 text-lg leading-8 text-dark-blue">Por la tecnologia de hoy y del futuro </p>
+                </div>
+            </div>
+            <div class="bg-turquese w-100 m-5 text-xs pl-8">
+                -
+            </div>
+            <div class="mx-auto mt-2 max-w-10xl sm:mt-20 lg:mt-8 lg:max-w-5xl">
+                <dl class="grid max-w-9xl grid-cols-2 gap-x-0 gap-y-10 lg:max-w-screen lg:grid-cols-4 lg:gap-y-0 border border-3 justify-stretch border-dark-blue">
+                    <div class="relative p-4 border border-3 justify-items-center border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Número de Control
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">21760550.</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Nombre
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">Kristina Gutmann.</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue ">
+                            Curp
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">GUXK030417MBCTMR02.</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Modalidad
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">Presencial.</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 col-start-1 col-end-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue ">
+                            Carrera
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">(04) ING. SIST COMPUTACIONALES</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 col-start-3 col-end-5 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Plan de Estudios
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">(3) ISIC-2010-224 DE 260 CREDITOS</dd>
+                    </div>
+                </dl>
+                <div class="mx-auto mt-2 max-w-10xl sm:mt-20 lg:mt-8 lg:max-w-5xl">
+                <dl class='grid max-w-9xl grid-cols-2 gap-x-0 gap-y-10 lg:max-w-screen lg:grid-cols-4 lg:gap-y-0 border border-3 justify-stretch border-dark-blue'>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Especialidad
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">Por Definir</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Tutor
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">N/A</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Correo Institucional
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">al21760550@ite.edu.mx</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Situacion
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">Vigente</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 col-start-1 col-end-3 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Periodo de Inicio
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">AGODIC - 2021.</dd>
+                    </div>
+                    <div class="relative p-4 border border-3 col-start-3 col-end-5 border-dark-blue">
+                        <dt class="text-base font-semibold leading-7 text-dark-blue">
+                            Periodo Actual o ultimo
+                        </dt>
+                        <dd class="mt-2 text-base leading-7 text-dark-blue">AGODIC - 2023</dd>
+                    </div>
+                </dl>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
